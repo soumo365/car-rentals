dac0aba0c39744f6652c4eb8336beb6274199d2b
tushar <duttatushar51@gmail.com>
2023-04-10 12:08:10 +0530

    footer script added

--- inc/footer.php
+++ inc/footer.php
@@ -5,7 +5,26 @@
 <script src="../js/bootstrap.min.js"></script>
 <script src="../js/jquery.min.js"></script>
 <script src="../js/jquery.slimNav_sk78.min.js"></script>
+<script src="../js/script.js"></script>
 <script src="../js/owl.carousel.js"></script>
+<script>
+$('.car-slide').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+</script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,22 @@
 <!-- @format -->
 
 <?php include 'inc/header.php'; ?>
-<main></main>
+<section class="car-slider">
+    <div class="container">
+        <div class="row">
+            <h3 class="heading-secondary">Luxury Car Rental Deals</h3>
+            <div class="col-lg-12">
+                <div class="car-slide owl-carousel owl-theme">
+                    <div class="item">
+                        <h4>1</h4>
+                    </div>
+                    <div class="item">
+                        <h4>2</h4>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
 <?php include 'inc/footer.php'; ?>
